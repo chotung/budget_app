9daf82a78ffabf9d5bdd659772b70e278884a2d2
chotung <38818833+chotung@users.noreply.github.com>
2023-10-24 12:05:53 -0400

    Updated workflow

--- budget_api_ts/src/plugins/postgres-plugin.ts
+++ budget_api_ts/src/plugins/postgres-plugin.ts
@@ -7,7 +7,8 @@ config()
 
 const plugin: FastifyPluginCallback = (fastify, options, done) => {
   const isProduction = process.env.NODE_ENV === "production"
-
+  // console.log(isProduction)
+  // console.log(process.env.DB_NAME);
   const pool = new Pool({
     connectionString: isProduction
       ? process.env.DATABASE_URL // Heroku-style environment variable in production
