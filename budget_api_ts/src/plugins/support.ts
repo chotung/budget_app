7a4fa5f4d8f700a5abb780951d7eccf0ecf7e453
chotung <38818833+chotung@users.noreply.github.com>
2023-10-27 14:31:13 -0400

    fix db connection
    DB plugin is now available as an instance of fastify

--- budget_api_ts/src/plugins/support.ts
+++ budget_api_ts/src/plugins/support.ts
@@ -1,20 +1,22 @@
-import fp from "fastify-plugin"
+import fp from "fastify-plugin";
+import { hash } from "bcrypt";
 
 export interface SupportPluginOptions {
   // Specify Support plugin options here
 }
 
-// The use of fastify-plugin is required to be able
-// to export the decorators to the outer scope
 export default fp<SupportPluginOptions>(async (fastify, opts) => {
-  fastify.decorate("someSupport", function () {
-    return "hugs"
-  })
-})
+  // Helper function: Hash a password securely
+  fastify.decorate("hashPassword", async function (password: string): Promise<string> {
+    const hashedPassword = await hash(password, 10); // Adjust the salt rounds as needed
+    return hashedPassword;
+  });
+});
 
 // When using .decorate you have to specify added properties for Typescript
 declare module "fastify" {
   export interface FastifyInstance {
-    someSupport(): string
+    hashPassword(password: string): Promise<string>;
+    // Add other helper functions here
   }
 }
